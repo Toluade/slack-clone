49e2fce3e72f4ef55b81e839993354f9358a9257
Tolu Adeoye <toluade1512@gmail.com>
2021-03-25 20:08:47 +0100

    connect app to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB8MjO3nkEEOsuSWAbKPopHGjonuF40ioo",
   authDomain: "slack-clone-toluade.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "129358500785",
   appId: "1:129358500785:web:83d3d1c675479f3703e390",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
