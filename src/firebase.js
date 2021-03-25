6f127cea73dc4fea38080045177ee5f5d33701c8
Tolu Adeoye <toluade1512@gmail.com>
2021-03-25 13:15:51 +0100

    Setup and build header component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB8MjO3nkEEOsuSWAbKPopHGjonuF40ioo",
+  authDomain: "slack-clone-toluade.firebaseapp.com",
+  projectId: "slack-clone-toluade",
+  storageBucket: "slack-clone-toluade.appspot.com",
+  messagingSenderId: "129358500785",
+  appId: "1:129358500785:web:83d3d1c675479f3703e390",
+};
