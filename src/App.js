e4dc694939b379f520061c1e2536b55431729e01
Tolu Adeoye <toluade1512@gmail.com>
2021-03-26 14:34:41 +0100

    Build Message component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import "./App.css";
 import Chat from "./components/Chat";
 import Header from "./components/Header.js";
 import Sidebar from "./components/Sidebar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <div className="app">
       <Router>
